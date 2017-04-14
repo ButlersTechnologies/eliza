05580150db17817a9f160b9658e3b2447446bfe5
cbutlerjr <cbutlerjr@users.noreply.github.com>
2017-04-13 18:24:55 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# eliza
+# Actions On Google Actions SDK Eliza Sample using Node.js
+
+This sample includes code from elizabot by N. Landsteiner, based on the original ELIZA program by Joseph Zeizenbaum (MIT 1966).
+
+## Setup Instructions
+
+### Pre-requisites
+ 1. Actions SDK: [https://developers.google.com/actions/develop/sdk/getting-started#getting-started](https://developers.google.com/actions/develop/sdk/getting-started#getting-started).
+ 1. Google Cloud SDK: [https://developers.google.com/actions/develop/sdk/getting-started#deploy-your-action-web-service](https://developers.google.com/actions/develop/sdk/getting-started#deploy-your-action-web-service).
+
+See the developer guide and release notes at [https://developers.google.com/actions/](https://developers.google.com/actions/) for more details.
+
+### Steps
+ 1. Deploy this action to your preferred hosting environment
+ (we recommend [Google Cloud Functions](https://cloud.google.com/functions/docs/tutorials/http)).
+ 1. Update the action package, eliza.json, with your endpoint URL.
+ 1. Preview the action using the gactions CLI: ./gactions preview --action_package eliza.json --invocation_name "eliza action" --preview_mins 1234
+ 1. Use the gactions simulator to test the action (try "talk to eliza action"): ./gactions simulate
+
+For more detailed information on deployment, see the [documentation](https://developers.google.com/actions/samples/).
+
+## References and How to report bugs
+* Actions on Google documentation: [https://developers.google.com/actions/](https://developers.google.com/actions/).
+* If you find any issues, please open a bug here on GitHub.
+* Questions are answered on [StackOverflow](https://stackoverflow.com/questions/tagged/actions-on-google).
+
+## How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md.
+
+## License
+See LICENSE.md.
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [https://g.co/actionsdev](https://g.co/actionsdev).
---
+++ index.js
@@ -0,0 +1,79 @@
+// Copyright 2016, Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
+const Eliza = require('elizabot');
+
+/**
+ * Handles the main intent coming from Assistant, when the user first engages with the action,
+ * asking for an initial prompt from Eliza.
+ */
+const mainIntentHandler = (assistant) => {
+  console.log('Main intent triggered.');
+  const eliza = new Eliza();
+  assistant.ask(eliza.getInitial(), {elizaInstance: eliza});
+};
+
+/**
+ * Handles the intent where the user sends a query to be handled by Eliza.
+ *
+ * This intent is triggered inside the dialogs when the user already has a conversation going on
+ * with Eliza, or when the user 'deep links' into the action by calling Eliza and already sending an
+ * initial prompt.
+ */
+const rawIntentHandler = (assistant) => {
+  console.log('Raw input intent triggered.');
+  const eliza = new Eliza();
+
+  // Reloads the previous instance of Eliza in case this is an ongoing conversation
+  const previousEliza = assistant.getDialogState().elizaInstance;
+  if (previousEliza) {
+    eliza.quit = previousEliza.quit;
+    eliza.mem = previousEliza.mem;
+    eliza.lastChoice = previousEliza.lastChoice;
+  }
+
+  const elizaReply = eliza.transform(assistant.getRawInput());
+  if (eliza.quit) {
+    assistant.tell(eliza.getFinal());
+  } else {
+    assistant.ask(elizaReply, {elizaInstance: eliza});
+  }
+};
+
+// Map that contains the intents and respective handlers to be used by the actions client library
+const actionMap = new Map();
+
+// Intent constants
+const RAW_INTENT = 'raw.input';
+
+/**
+ * Configures the post request handler by setting the intent map to the right functions.
+ */
+actionMap.set(new ActionsSdkAssistant().StandardIntents.MAIN, mainIntentHandler);
+actionMap.set(RAW_INTENT, rawIntentHandler);
+actionMap.set(new ActionsSdkAssistant().StandardIntents.TEXT, rawIntentHandler);
+
+/**
+ * Handles the post request incoming from Assistant.
+ */
+exports.eliza = (req, res) => {
+  console.log('Incoming post request...');
+  const assistant = new ActionsSdkAssistant({request: req, response: res});
+  assistant.handleRequest(actionMap);
+};
